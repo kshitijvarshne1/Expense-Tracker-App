4256322c7024a18f43bdede6fb015599db6413b4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-07-24 22:25:52 +0530

    add dynamic transaction

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,18 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 export const TransactionList = () => {
+  const { transaction } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transaction.map((transaction) => (
+          <li className="minus">
+            {transaction.text} <span>-$400</span>
+            <button className="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
   );
