f2ce6652d3d8214405f5434d695446ebf1c6d1b0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-07-25 22:20:29 +0530

    add condition for posi and negi

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <li className="minus">
       {transaction.text} <span>-$400</span>
